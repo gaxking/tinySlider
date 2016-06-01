a01b7b18bb40bfc15455a85c408fcb652a76ac6d
gaxking <gaxking@gmail.com>
2016-06-01 15:54:57 +0800

    功能完善

--- tinySlider.js
+++ tinySlider.js
@@ -1,72 +1,61 @@
 ;(function(w){
 	//兼容处理
-	var	transName=(function(){
+	var	transName=(function() {
 		var transEndEventNames = {
 			WebkitTransition : 'webkitTransitionEnd',
 			transition       : 'transitionend'
-			}
+		};
 
 		var transitionNames = {
-			WebkitTransition : ['webkitTransition','-webkit-transition'],
-			transition       : ['transition','transition']
-			}
+			WebkitTransition : ['webkitTransition', '-webkit-transition'],
+			transition       : ['transition', 'transition']
+		};
 
 		var transformNames = {
-			WebkitTransition : ['webkitTransform','-webkit-transform'],
-			transition       : ['transform','transform']
-			}
-
+			WebkitTransition : ['webkitTransform', '-webkit-transform'],
+			transition       : ['transform', 'transform']
+		};
 
 		var style = document.body.style;
-		for(var name in transEndEventNames){
-			if(typeof style[name] === "string"){
+		for(var name in transEndEventNames) {
+			if(typeof style[name] === "string") {
 				return {
 					transEndEven:transEndEventNames[name],
 					transition:transitionNames[name],
-					transform:transformNames[name],
-				}
+					transform:transformNames[name]
+				};
 			}
 		}
 	})();
 
-	//公共变量
-	function autoFire(clientWidth){
-		var _this = this;
-		this.timer = setTimeout(function(){
-			_this.index  = _this.options.total - 1 == _this.index ? 0 : ++_this.index;
-			addDotFn.call(_this);
-			fire(_this.options.dom, clientWidth * _this.index);
-		},this.options.stay);
-	}
-
 	//处理点点点
-	function addDotFn(){
-		if(this.options.addDot){
+	function addDotFn() {
+		if(this.options.addDot) {
 			this.options.dots = Array.prototype.slice.call(this.options.dots);
-			for(x in this.options.dots){
+			for(var x in this.options.dots) {
 				this.options.dots[x].style.backgroundColor = '#ddd';
 			}
 			this.options.dots[this.index].style.backgroundColor = '#0ae';
 		}
 	}
 
-	function fire(ele,x){
+	function fire(ele, x) {
 		ele.style[transName.transform[0]] = 'translate3d('+ (0-x) +'px, 0px, 0px)';
 	}
 
-	var TinySlider = function(param){
+	var TinySlider = function(param) {
 		var _this = this,
-		options = this.options = {
-			list:param.list,
-			ele:document.querySelector(param.selector),
-			stay:param.stay || 2000,
-			duration:param.duration || 300,
-			total:param.list.length,
-			addDot:param.addDot || true,
-			isCustom:param.isCustom || false,
-			dom:null,	//els下的ui
-			dots:null   //是否加入点
-		}
+			options = this.options = {
+				list:param.list,
+				ele:document.querySelector(param.selector),
+				stay:param.stay || 2000,
+				duration:param.duration || 300,
+				total:param.list.length,
+				addDot:param.addDot || true,
+				isCustom:param.isCustom || false,
+				dom:null,	//els下的ui
+				dots:null   //是否加入点
+			};
 
 		//私有变量
 		var	moveX = 0,
@@ -78,27 +67,28 @@
 			html = '<ul style="height:100%;width:'+clientWidth*options.total+'px">';
 
 		//组装html
-		if(options.isCustom){
-			for(var i = 0; i < options.total; i++){
+		var i;
+		if(options.isCustom) {
+			for(i = 0; i < options.total; i++) {
 				html += '<li style="display:inline-block;width:'+clientWidth+'px;height:100%">'+options.list[i]+'</li>';
 			}
 		}else{
-			for(var i = 0; i < options.total; i++){
+			for(i = 0; i < options.total; i++) {
 				html += '<li style="display:inline-block;width:'+clientWidth+'px;height:100%"><img style="width:100%;height:100%" src="'+options.list[i]+'"></li>';
 			}
 		}
 		html += '</ul>';
 		//组装dot
-		if(options.addDot){
+		if(options.addDot) {
 			html += '<div style="text-align:center;'+transName.transform[1]+':translateY(-100%);padding:0.1rem 0">';
-			for(var i = 0; i < options.total; i++){
+			for(i = 0; i < options.total; i++) {
 				html += '<span style="display:inline-block;padding:.07rem 0 0 .07rem;border-radius:50%;background-color:#ddd;margin:0 .025rem" index="0"></span>';
 			}
 			html += '</div>';
 		}
 
 		options.ele.innerHTML = html;
-		var dom = options.dom = options.ele.children[0],s;
+		var dom = options.dom = options.ele.children[0], s;
 		options.dots = options.ele.children[1].children;
 
 		
@@ -108,16 +98,15 @@
 
 		//未来让distroy保持关联，将方法写在this中
 		this.Events = {};
-		this.Events['touchstart'] = function(e){
+		this.Events['touchstart'] = function(e) {
 			s = (new Date()).getTime();
 
 			startX = e.targetTouches[0].clientX;
 			startY = e.targetTouches[0].clientY;
-			clearTimeout(_this.timer);
+			clearTimeout(_this.heartTimer);
 		};
-		this.Events['touchmove'] = function(e){
+		this.Events['touchmove'] = function(e) {
 			dom.style[transName.transition[0]] = null;
-			clearTimeout(_this.timer);
 
 			//移动x的距离
 			moveX = startX - e.targetTouches[0].clientX;
@@ -126,37 +115,45 @@
 			moveY = e.targetTouches[0].clientY - startY;
 			startY = e.targetTouches[0].clientY;
 
-			window.scroll(0,document.body.scrollTop - moveY);
+			var y = document.body.scrollTop - moveY<0?0:document.body.scrollTop - moveY;
+
+			window.scroll(0, y);
 
 			//滑动总距离
-			clientX =  _this.index * clientWidth + moveX;
-			if(clientX<0){
+			clientX = _this.index * clientWidth + moveX;
+			if(clientX<0) {
 				clientX = clientX/2;
-			}else if(clientX > (options.total-1) * clientWidth){
+			}else if(clientX > (options.total-1) * clientWidth) {
 				clientX = clientX - (clientX - clientWidth * (options.total-1))/2;
 			}
 
 			fire(dom, clientX);
 			e.preventDefault();
-		}
-		this.Events[transName.transEndEven] = function(){
-			autoFire.call(_this,clientWidth);
-		}
+		};
 
-		this.Events['touchend'] = function(e){
+		this.Events['heartTimer'] = function() {
+			_this.heartTimer = setInterval(function() {
+				//autoFire.call(_this, clientWidth);
+				_this.index = _this.options.total - 1 == _this.index ? 0 : ++_this.index;
+				addDotFn.call(_this);
+				fire(_this.options.dom, clientWidth * _this.index);
+			}, options.stay + options.duration);
+		};
+
+		this.Events['touchend'] = function() {
 			dom.style[transName.transition[0]] = "all "+options.duration/1000+"s ease";
 			var b = clientX - _this.index * clientWidth;
-			if(b<0){
-				if((new Date()).getTime()-s>200){
+			if(b<0) {
+				if((new Date()).getTime()-s>200) {
 					_this.index = b < clientWidth/2?_this.index-1:_this.index;
-				}else{
+				}else if(moveX < -15) {
 					_this.index--;
 				}
 				_this.index = _this.index < 0 ? 0 : _this.index;
 			}else{
-				if((new Date()).getTime()-s>200){
+				if((new Date()).getTime()-s>200) {
 					_this.index = b > clientWidth/2?_this.index+1:_this.index;
-				}else{
+				}else if(moveX > 15) {
 					_this.index++;
 				}
 
@@ -168,25 +165,33 @@
 
 			addDotFn.call(_this);
 			fire(dom, clientX);
-		}
+			_this.Events['heartTimer']();
+		};
 
 		//触摸事件
-		dom.addEventListener('touchstart',this.Events['touchstart']);
-		dom.addEventListener('touchmove',this.Events['touchmove']);
-		dom.addEventListener('touchend',this.Events['touchend']);
+		dom.addEventListener('touchstart', this.Events['touchstart']);
+		dom.addEventListener('touchmove', this.Events['touchmove']);
+		dom.addEventListener('touchend', this.Events['touchend']);
 
-		//动画结束事件
-		dom.addEventListener(transName.transEndEven,this.Events[transName.transEndEven]);
-		autoFire.call(this,clientWidth);
-	}
+		//运行心跳定时器
+		this.Events['heartTimer']();
+	};
 
 	//销毁插件
-	TinySlider.prototype.destroy = function(){
-		this.options.dom.removeEventListener('touchstart',this.Events['touchstart']);
-		this.options.dom.removeEventListener('touchmove',this.Events['touchmove']);
-		this.options.dom.removeEventListener('touchend',this.Events['touchend']);
-		this.options.dom.removeEventListener(transName.transEndEven,this.Events[transName.transEndEven]);
-	}
+	TinySlider.prototype.destroy = function() {
+		this.options.dom.removeEventListener('touchstart', this.Events['touchstart']);
+		this.options.dom.removeEventListener('touchmove', this.Events['touchmove']);
+		this.options.dom.removeEventListener('touchend', this.Events['touchend']);
+		clearTimeout(this.heartTimer);
+	};
+
+	//销毁插件
+	TinySlider.prototype.destroy = function() {
+		this.options.dom.removeEventListener('touchstart', this.Events['touchstart']);
+		this.options.dom.removeEventListener('touchmove', this.Events['touchmove']);
+		this.options.dom.removeEventListener('touchend', this.Events['touchend']);
+		clearTimeout(this.heartTimer);
+	};
 
 	//暴露接口
 	w.TinySlider = TinySlider;
